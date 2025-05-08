fa0a056ae5daf29e30e4f8b7c6f37ea7428b1a89
Qurbonoff <159319793+Qurbonoff11@users.noreply.github.com>
2025-05-08 10:09:40 +0500

    add audio

--- js/script.js
+++ js/script.js
@@ -6,33 +6,35 @@ let calcNumber = $HTML.querySelector(".screen-num");
 let resetBtn = $HTML.querySelector(".res-btn");
 let plusBtn = $HTML.querySelector(".plus-btn");
 
+let rington = new Audio("../assets/audio/ring.mp3");
+
 let num = 0;
 calcNumber.innerHTML = num;
 plusBtn.addEventListener("click", () => {
   calcNumber.innerHTML++;
 
   if (parseInt(calcNumber.innerHTML) >= 34) {
-    calcNumber.innerHTML = 0
+    calcNumber.innerHTML = 0;
   }
 });
 
 $HTML.addEventListener("keydown", (event) => {
-
   if (event.key === "+" && parseInt(calcNumber.innerHTML) >= 0) {
     calcNumber.innerHTML++;
-    if (calcNumber.innerHTML >= 34) {
-      calcNumber.innerHTML = 0
+    if (calcNumber.innerHTML >= 33) {
+      calcNumber.innerHTML = 0;
+      rington.play();
     }
   }
 });
 
 resetBtn.addEventListener("click", () => {
-  calcNumber.innerHTML = 0
-})
+  calcNumber.innerHTML = 0;
+});
 
 $HTML.addEventListener("keydown", (even) => {
   if (even.key === "0" && parseInt(calcNumber.innerHTML) > 0) {
-    calcNumber.innerHTML = 0
+    calcNumber.innerHTML = 0;
     console.log(even.key);
   }
-})
+});
